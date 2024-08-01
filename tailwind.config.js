5d3ae8447d07d721e5d12b08e72b7327fa519376
JosieEsi <josephine.kpodo@amalitech.org>
2024-08-01 21:28:58 +0000

    updated the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ export default {
         "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
       },
       backgroundImage: {
-        hero: "url('/assets/images/hero.svg')",
+        hero: "url('../assets/images/hero.svg')",
       },
       screens: {
         wide: "1440px",
